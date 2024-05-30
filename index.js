b608e5d81b82346dd4755f3ce82a35ff61c42326
Naku <71041457+Naku-git@users.noreply.github.com>
2024-05-29 21:12:48 -0700

    Initial Push

--- index.js
+++ index.js
@@ -9,87 +9,88 @@ var neighbornamelist = [];
 var tidgen = 1
 var sqllist = [];
 
-const territorypromise = new Promise((resolve, reject) => [
+const territorypromise = new Promise((resolve, reject) => {
     fs.readFile('map.svg', function(err, data) {
+        if (err) {
+            console.error("Error reading file:", err);
+            return;
+        }
         const parsed = parse(data.toString());
         var territorycontainer = [];
+        var territoriesFound = false;
+        var id = 'Territories';
         for (const i of parsed.children[0].children) {
-            if (i.properties.id === 'Territories') {
-                var namepath = i.children;
-                for (const j of namepath) {
-                    var territorydump;
-                    territorydump = {name: j.properties.id, id: tidgen, region: j.properties.region}
-                    territorycontainer.push(territorydump);
-                    neighbornamelist.push(j.properties.id);
-                    tidgen = tidgen + 1;
+            if (i.properties.id === id) {
+                territoriesFound = true;
+                var regions = i.children;
+                for (const region of regions) {
+                    var regionName = region.properties.id;
+                    var states = region.children;
+                    for (const state of states) {
+                        var territorydump;
+                        territorydump = {name: state.properties.id, id: tidgen, region: regionName}
+                        territorycontainer.push(territorydump);
+                        neighbornamelist.push(state.properties.id);
+                        tidgen = tidgen + 1;
+                    }
                 }
+                console.log(territorycontainer);
                 resolve(territorycontainer);
+                
             }
         }
+        if (!territoriesFound) {
+            console.error("Error: No " + id + " element found in the SVG data.");
+            process.exit(1);
+        }
     })
-])
+})
+
+
+
 
 const timer = (ms) => new Promise(res => setTimeout(res, ms))
-const terrprompt = (terrname) => new Promise((resolve, reject) => [
-    inquirer.prompt([
+
+const terrprompt = async (terrname) => {
+    const answers = await inquirer.prompt([
         {
             type: 'input',
             name: terrname,
             message: `List the neighbors of the territory ${terrname} (case insensitive, only list neighbors)`,
             validate(answer) {
-                        tempanscont = [];
-                        neighbors = answer.split(" ");
-                        for (const j of neighbors) {
-                            for (const i of neighbornamelist) {
-                                if (j.toLowerCase() === i.toString().toLowerCase()) {
-                                    tempanscont.push(i);
-                                    break;
-                                }
-                            }
-                        }
-                        if (tempanscont.length === neighbors.length) {
-                            for (const i of tempanscont) {
-                                neighborcontainer.push([terrname, i]);
-                            }
-                            neighborcontainer.push([terrname, terrname]);
-                            return true;
-                        } else {
-                        return 'One of the territories you entered either does not exist or was typed incorrectly (extra space somewhere also possible), please try again';
-                        }
+                if (tempanscont.length === neighbors.length) {
+                    for (const i of tempanscont) {
+                        neighborcontainer.push([terrname, i]);
                     }
-        }
-    ]).then(answers => {
-            neighbors = JSON.stringify(answers)
-            inquirer.prompt([
-                {
-                    type: 'confirm',
-                    name: 'terrconfirmation',
-                    message: `You've inputted the following as neighbors of ${neighbors} ... is this correct?`
+                    neighborcontainer.push([terrname, terrname]);
+                    return true;
+                } else {
+                return 'One of the territories you entered either does not exist or was typed incorrectly (extra space somewhere also possible), please try again';
                 }
-            ])
-            .then( ans => {
-                if (ans.terrconfirmation === true) {
-                    resolve(true);
-                }
-                if (ans.terrconfirmation === false) {
-                    resolve(false);
-                }
-            })
-    })
-])
-
-territorypromise.then( async (tcs) => {
-    for (const i of tcs) {
-        async function checkterr(name) {
-            var terr = await terrprompt(name);
-            if (terr === true) {
-                return;
-            } else if (terr === false) {
-                await checkterr(name);
-                return;
             }
         }
-        await checkterr(i.name);
+    ]);
+
+    const neighbors = JSON.stringify(answers);
+
+    const confirmation = await inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'terrconfirmation',
+            message: `You've inputted the following as neighbors of ${neighbors} ... is this correct?`
+        }
+    ]);
+
+    return confirmation.terrconfirmation;
+}
+
+
+territorypromise.then(async (tcs) => {
+    for (const i of tcs) {
+        const terr = await terrprompt(i.name);
+        if (!terr) {
+            await terrprompt(i.name);
+        }
         await timer(500);
     }
     for (const s of neighborcontainer) {
